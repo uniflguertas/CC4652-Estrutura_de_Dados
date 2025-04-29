7f4c8664cf52a7cb7aeca955287712623176cd8d
Luís Miguel <149299894+uniflguertas@users.noreply.github.com>
2025-04-29 17:53:12 -0300

    Update cadastro.c
    Funções de criação de Lista e ELista.

--- src/cadastro.c
+++ src/cadastro.c
@@ -1,1 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include "../include/cadastro.h"
+#include "../include/clearscreen.h"
 
+Lista *criar_lista() {
+    Lista *lista = malloc(sizeof(Lista));
+    lista->primeiro = NULL;
+    lista->qtde = 0;
+    return lista;
+}
+
+ELista *criar_Elista(Registro *registro) {
+    ELista *elista = malloc(sizeof(ELista));
+    elista->proximo = NULL;
+    elista->dados = registro;
+    return elista;
+}
+
+}
