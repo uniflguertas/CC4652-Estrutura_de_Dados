11f283d271aac392ae4c655466f4317e9ca9b9ab
Luís Miguel <149299894+uniflguertas@users.noreply.github.com>
2025-04-29 14:11:02 -0300

    Criação sobre.c
    Criação da função sobre() para o menu.

---
+++ sobre.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include "include/sobre.h"
+
+void sobre() {
+    printf("================================================== \n");
+    printf("CC4652 - Estrutura de Dados\n");
+    printf("Projeto: Serviço de Atendimento de Saúde\n");
+    printf("================================================== \n");
+    printf("CRÉDITOS: \n");
+    printf("Luís Miguel de Almeida Guertas - RA 24.123.040-8 \n");
+    printf("4º Ciclo de Ciência da Computação \n");
+    printf("28/04/2025 - ~ \n");
+    printf("================================================== \n");
+}
