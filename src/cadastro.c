5c9329f76e638425c8bb184a68475ec2c246ca0c
Luís Miguel <149299894+uniflguertas@users.noreply.github.com>
2025-04-29 18:00:20 -0300

    Implementação do menu de cadastro no menu principal

--- src/cadastro.c
+++ src/cadastro.c
@@ -2,45 +2,53 @@
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
+#include "../include/sobre.h"
 #include "../include/cadastro.h"
 #include "../include/clearscreen.h"
 
-Lista *criar_lista() {
-    Lista *lista = malloc(sizeof(Lista));
-    lista->primeiro = NULL;
-    lista->qtde = 0;
-    return lista;
+/*
+Função de retorno ao menu principal.
+*/
+void voltarMenu(void) {
+    printf("\n Pressione ENTER para voltar ao menu principal. ");
+    getchar();
+    getchar();
 }
 
-ELista *criar_Elista(Registro *registro) {
-    ELista *elista = malloc(sizeof(ELista));
-    elista->proximo = NULL;
-    elista->dados = registro;
-    return elista;
-}
-
-int menuitem_cadastro(void) {
+/*
+main(): imprime o menu e seleciona as opções.
+*/
+int main(void) {
+     // index: seletor de opção.
     int index;
-    
+
+    clearScreen();
+
+ // do/while(): executa o print do menu enquanto index diferente de 8.
     do {
+        printf("Seja bem-vindo ao Serviço de Atendimento de Saúde. \n");
+        printf("O que gostaria de fazer hoje? \n");    
         printf("================================================== \n");
         printf("Digite o número da opção desejada abaixo: \n");
-        printf("1. Cadastrar cliente\n");
-        printf("2. Consultar paciente\n");
-        printf("3. Consultar lista de pacientes\n");
-        printf("4. Alterar dados de um cliente\n");
-        printf("5. Remover cadastro de cliente\n");
-        printf("6. Salvar\n");
-        printf("================================================== \n");
-        printf("Insira 7 para voltar ao menu principal.\n");
-        printf("Digite aqui: ");
+        printf("\n");
+        printf("1. Cadastrar\n");
+        printf("2. Atendimento\n");
+        printf("3. Atendimento Prioritário\n");
+        printf("4. Pesquisa\n");
+        printf("5. Desfazer\n");
+        printf("6. Carregar/Salvar\n");
+        printf("7. Sobre\n");
+        printf("8. Sair\n");
+        printf("==================================================");
+
+        printf("\nDigite aqui: ");
         scanf("%d", &index);
 
-        switch(index) {
+     // switch(): seleção de opção.
+        switch (index) {
             case 1: {
                 clearScreen();
-                printf("\nOpção 1 selecionada.\n \n");
-                sleep(1);
+                menuitem_cadastro();
                 clearScreen();
                 break;
             }
@@ -81,10 +89,30 @@ int menuitem_cadastro(void) {
             }
             case 7: {
                 clearScreen();
+                sobre();
+                voltarMenu();
+                clearScreen();
                 break;
             }
-        }
+            case 8: {
+                break;
+            }
+            default: {
+                clearScreen();
+                printf("\n404 - Not Found\n \n");
+                sleep(1);
+                clearScreen();
+                break;
+            }
+        };
+    }
+    while (index != 8);
 
-    } while(index != 7);
+ // Print quando encerra o programa.
+    clearScreen();
+    printf("\n Encerrando... \n \n");
+    sleep(1);
+    clearScreen();
+    
     return 0;
 }
